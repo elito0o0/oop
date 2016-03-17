088ebe79f6ea8356e291890116014508b8620f62
elito0o0 <elito0o097@abv.bg>
2016-03-17 11:53:43 +0200

    Create planet.cpp

---
+++ 13.03.2016/planet.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+//skorostta na svetlinata 2.99...792km/s
+struct Planet
+{
+	char name[20];
+	double diameter;
+	double mass;
+
+	void read();
+	void print();
+	double GetTimeForLightTravel(double dist);
+};
+void read()
+
+int main()
+{
+	Planet p[30];
+
+}
